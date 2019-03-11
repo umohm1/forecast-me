0752ecab3fceff01fbb4a3193858e36609211157
Mary <umohm12@gmail.com>
2019-03-11 12:27:57 -0400

    Format API response, add icons and date

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import ReactAnimatedWeather from 'react-animated-weather';
+import moment from 'moment';
 import { handleResponse } from '../helpers';
+
+let defaults = {
+  icon: 'CLEAR_DAY',
+  color: 'goldenrod',
+  size: 512,
+  animate: true
+};
 
 class Forecast extends React.Component {
   constructor() {
@@ -13,19 +22,17 @@ class Forecast extends React.Component {
 
   componentDidMount() {
     const { zipcode } = this.props.match.params;
-    console.log(
-      `${process.env.REACT_APP_API}/weather?zip=${zipcode}&appid=${
-        process.env.REACT_APP_API_KEY
-      }&units=imperial`
-    );
-    fetch(
-      `${ process.env.REACT_APP_API
-      }/weather?zip=${zipcode}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)
+    
+    fetch(`${ process.env.REACT_APP_API}/weather?zip=${zipcode}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)
       .then(handleResponse)
       .then(forecastData => {
-        console.log('====api response', forecastData);
         this.setState({
-          forecastData,
+          forecastData: {
+            name: forecastData.name,
+            icon: forecastData.weather[0].icon,
+            temp: forecastData.main.temp,
+            description: forecastData.weather[0].description
+          },
           error: null
         });
       })
@@ -36,18 +43,59 @@ class Forecast extends React.Component {
       });
   }
 
-  componentWillReceiveProps() {
-
+  extractIcon = () => {
+    switch (this.state.forecastData.icon) {
+      case '01d':
+        defaults.icon = 'CLEAR_DAY';
+        defaults.color = 'goldenrod';
+        break;
+      case '01n':
+        defaults.icon = 'CLEAR_NIGHT';
+        defaults.color = 'goldenrod';
+        break;
+      case '02d':
+        defaults.icon = 'PARTLY_CLOUDY_DAY';
+        defaults.color = 'goldenrod';
+        break;
+      case '02n':
+        defaults.icon = 'PARTLY_CLOUDY_NIGHT';
+        defaults.color = 'goldenrod';
+        break;
+      case '03d':
+        defaults.icon = 'CLOUDY';
+        defaults.color = 'goldenrod';
+        break;
+      case '03n':
+        defaults.icon = 'CLOUDY';
+        defaults.color = 'goldenrod';
+        break;
+      default:
+        defaults.icon = 'RAIN';
+        defaults.color = 'goldenrod';
   }
+}
+
 
     render() {
-      console.log(this.props)
       const { forecastData } = this.state;
-      console.log("The forecast data is here: ", forecastData);
+
+      if (!Object.keys(forecastData)){
+        return <div>Loading.....</div>
+      }
+      this.extractIcon()
+      console.log(defaults)
       return (
         <div>
-          <h1></h1>
-          
+          <h1>{forecastData.name}</h1>
+          <ReactAnimatedWeather
+            icon={defaults.icon}
+            color={defaults.color}
+            size={defaults.size}
+            animate={defaults.animate}
+          />
+          <p>{forecastData.temp}</p>
+          <p>{forecastData.description}</p>
+          <p>{moment().format('MMMM Do YYYY')}</p>
         </div>
       );
     }
