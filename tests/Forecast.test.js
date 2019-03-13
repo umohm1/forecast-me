f7599cd5be77165f68f48d6016d6317e57f5012e
Mary <umohm12@gmail.com>
2019-03-13 12:23:10 -0400

    Working on Forecast test

--- tests/Forecast.test.js
+++ tests/Forecast.test.js
@@ -4,16 +4,14 @@ import expect from 'expect';
 import Forecast from '../src/components/Forecast';
 
 describe('<Forecast />', () => {
-  const props = {...forecastData};
-const wrapper = mount(<Forecast{...props} />);
-  it('should check that p tags exists', () => {
+  it('should check that p tags exist', () => {
+    const wrapper = mount(
+      <Forecast
+        forecastData={{ icon: 'CLEAR_DAY' }}
+        zipCodeData={{ State: '' }}
+      />
+    ); 
     expect(wrapper.find('p').length).toBe(3);
   });
 });
 
-// describe('<Forecast />', () => {
-//   it('should check that p tags exist', () => {
-//     const wrapper = mount(<Forecast />);
-//     expect(wrapper.find('p').length).toBe(3);
-//   });
-// });
